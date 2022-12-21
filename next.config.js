4a5a4763bae8e7080301c9cf4a5cf8fdb82d9c31
Paul Jaworski <paulmattjaws@gmail.com>
2022-12-21 20:42:09 +0000

    add next/image examples

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,58 @@
+import Image from 'next/image';
+
+import styles from '../styles/images.module.css';
+import localPic from '../images/photo-1670969548019-18ec1aae8abe.jpeg';
+
+const fillItems = [
+  {
+    src: 'https://images.unsplash.com/photo-1671227498016-93aa927686f8',
+    alt: '',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1670969548019-18ec1aae8abe',
+    alt: '',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1671296368370-a24a6f9148cf',
+    alt: '',
+  },
+];
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.fillItems}>
+          {fillItems.map((fillItem, index) => (
+            <div className={styles.fillItem} key={index}>
+              <div className={styles.fillWrapper}>
+                <Image
+                  src={fillItem.src}
+                  alt={fillItem.alt}
+                  fill
+                  sizes="(min-width: 48em) calc(50vw - 6rem), (min-width: 64em) calc(33vw - 6rem), (min-width: 79em) 375px, calc(100vw - 4rem)"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.intrinsicItem}>
+          <Image
+            src="https://images.unsplash.com/photo-1670969548019-18ec1aae8abe"
+            alt=""
+            width={800}
+            height={533}
+          />
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.intrinsicItem}>
+          <Image src={localPic} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
